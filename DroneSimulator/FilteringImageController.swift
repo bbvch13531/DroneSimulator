c7be641eb4fb77f67f7d67979315fe2e2d4f9557
bbvch13531 <bbvch13531@gmail.com>
2018-09-11 21:07:44 +0900

    Enable api request
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- DroneSimulator/FilteringImageController.swift
+++ DroneSimulator/FilteringImageController.swift
@@ -13,7 +13,7 @@ class FilteringImageController: UIViewController {
 	var imgView = UIImageView()
 	var filenameLabel = UILabel()
 	var imageId = UILabel()
-	var imageIdField = UITextField()
+	var imageIdFromData = UILabel()
 	var pointNumber = UILabel()
 	var pointNumberField = UITextField()
 	var leafSize = UILabel()
@@ -48,7 +48,7 @@ class FilteringImageController: UIViewController {
 		print("userDefault = \(userDefault.value(forKey: "imgData"))")
 		userDefault.addSuite(named: "group.DroneSimulator")
 		if let dictabc = userDefault.value(forKey: "imgData")  {
-			var dict = dictabc as! NSDictionary
+			let dict = dictabc as! NSDictionary
 //			print("dict=\(dict)")
 			let data = dict.value(forKey: "imgData") as! Data
 			let str = dict.value(forKey: "name") as! String
@@ -95,11 +95,15 @@ class FilteringImageController: UIViewController {
 		leafSize.textAlignment = .center
 		widthSize.textAlignment = .center
 		
-		var submitBtn = UIButton(frame: CGRect(x: 300, y: 200, width: 200, height: 100))
+		var submitBtn = UIButton()
 		submitBtn.setTitle("Submit", for: .normal)
 		submitBtn.addTarget(self, action: #selector(submit), for: .touchUpInside)
-		submitBtn.tintColor = UIColor.black
-		submitBtn.backgroundColor = UIColor.blue
+		submitBtn.tintColor = UIColor.white
+		submitBtn.backgroundColor = self.view.tintColor
+		submitBtn.layer.cornerRadius = 10
+		
+		
+		setBorderToTextField()
 		
 		imageId.text = "Image ID :"
 		pointNumber.text = "Number of point :"
@@ -110,7 +114,7 @@ class FilteringImageController: UIViewController {
 		filenameLabel.translatesAutoresizingMaskIntoConstraints = false
 		submitBtn.translatesAutoresizingMaskIntoConstraints = false
 		imageId.translatesAutoresizingMaskIntoConstraints = false
-		imageIdField.translatesAutoresizingMaskIntoConstraints = false
+		imageIdFromData.translatesAutoresizingMaskIntoConstraints = false
 		pointNumber.translatesAutoresizingMaskIntoConstraints = false
 		pointNumberField.translatesAutoresizingMaskIntoConstraints = false
 		leafSize.translatesAutoresizingMaskIntoConstraints = false
@@ -122,13 +126,19 @@ class FilteringImageController: UIViewController {
 		self.scrollView.addSubview(filenameLabel)
 		self.scrollView.addSubview(submitBtn)
 		self.scrollView.addSubview(imageId)
-		self.scrollView.addSubview(imageIdField)
+		self.scrollView.addSubview(imageIdFromData)
 		self.scrollView.addSubview(pointNumber)
 		self.scrollView.addSubview(pointNumberField)
 		self.scrollView.addSubview(leafSize)
 		self.scrollView.addSubview(leafSizeField)
 		self.scrollView.addSubview(widthSize)
 		self.scrollView.addSubview(widthSizeField)
+		
+		submitBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+		submitBtn.widthAnchor.constraint(equalToConstant: 150).isActive = true
+		
+		submitBtn.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor).isActive = true
+		submitBtn.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor, constant: 250).isActive = true
 		
 		imgView.heightAnchor.constraint(equalToConstant: 300).isActive = true
 		imgView.widthAnchor.constraint(equalToConstant: 300).isActive = true
@@ -141,19 +151,36 @@ class FilteringImageController: UIViewController {
 		setLabelSizeTo(label: pointNumber, width: 300, height: 150)
 		setLabelSizeTo(label: leafSize, width: 300, height: 150)
 		setLabelSizeTo(label: widthSize, width: 300, height: 150)
+		setLabelSizeTo(label: imageIdFromData, width: 300, height: 150)
 		
-		setTextFieldSizeTo(textField: imageIdField, width: 300, height: 150)
-		setTextFieldSizeTo(textField: pointNumberField, width: 300, height: 150)
-		setTextFieldSizeTo(textField: leafSizeField, width: 300, height: 150)
-		setTextFieldSizeTo(textField: widthSizeField, width: 300, height: 150)
+		
+		setTextFieldSizeTo(textField: pointNumberField, width: 150, height: 40)
+		setTextFieldSizeTo(textField: leafSizeField, width: 150, height: 40)
+		setTextFieldSizeTo(textField: widthSizeField, width: 150, height: 40)
 		
 		filenameLabel.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor).isActive = true
 		filenameLabel.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor, constant: -50).isActive = true
+		setConstraintToLable(label: imageId, xConstant: -100, yConstant: 0)
+		setConstraintToLable(label: pointNumber, xConstant: -100, yConstant: 50)
+		setConstraintToLable(label: leafSize, xConstant: -100, yConstant: 100)
+		setConstraintToLable(label: widthSize, xConstant: -100, yConstant: 150)
+		setConstraintToLable(label: imageIdFromData, xConstant: 100, yConstant: 0)
 		
+		setConstraintToTextField(textField: pointNumberField, xConstant: 100, yConstant: 50)
+		setConstraintToTextField(textField: leafSizeField, xConstant: 100, yConstant: 100)
+		setConstraintToTextField(textField: widthSizeField, xConstant: 100, yConstant: 150)
 	}
 	
 	@objc func submit(sender:UIButton){
-		
+		print("submit")
+	}
+	func setConstraintToLable(label: UILabel,xConstant: CGFloat, yConstant: CGFloat){
+		label.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor, constant: xConstant).isActive = true
+		label.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor, constant: yConstant).isActive = true
+	}
+	func setConstraintToTextField(textField: UITextField,xConstant: CGFloat, yConstant: CGFloat){
+		textField.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor, constant: xConstant).isActive = true
+		textField.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor, constant: yConstant).isActive = true
 	}
 	func setLabelSizeTo(label: UILabel, width: CGFloat, height: CGFloat){
 		label.widthAnchor.constraint(equalToConstant: width).isActive = true
@@ -162,5 +189,22 @@ class FilteringImageController: UIViewController {
 	func setTextFieldSizeTo(textField: UITextField, width: CGFloat, height: CGFloat){
 		textField.widthAnchor.constraint(equalToConstant: width).isActive = true
 		textField.heightAnchor.constraint(equalToConstant: height).isActive = true
+	}
+	func setBorderToTextField(){
+//		imageIdField.layer.borderColor = UIColor.gray.cgColor
+//		imageIdField.layer.borderWidth = 1
+//		imageIdField.layer.cornerRadius = 8.0
+		
+		pointNumberField.layer.borderColor = UIColor.gray.cgColor
+		pointNumberField.layer.borderWidth = 1
+		pointNumberField.layer.cornerRadius = 8.0
+		
+		leafSizeField.layer.borderColor = UIColor.gray.cgColor
+		leafSizeField.layer.borderWidth = 1
+		leafSizeField.layer.cornerRadius = 8.0
+		
+		widthSizeField.layer.borderColor = UIColor.gray.cgColor
+		widthSizeField.layer.borderWidth = 1
+		widthSizeField.layer.cornerRadius = 8.0
 	}
 }
--- ImageExportor/ShareViewController.swift
+++ ImageExportor/ShareViewController.swift
@@ -13,69 +13,46 @@ import Alamofire
 class ShareViewController: SLComposeServiceViewController {
 	
 	override func isContentValid() -> Bool {
-// Do validation of contentText and/or NSExtensionContext attachments here
+		// Do validation of contentText and/or NSExtensionContext attachments here
 		return true
 	}
 	
 	override func didSelectPost() {
-// This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-
-// Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+		// This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+		
+		// Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
 		
 		if let item = self.extensionContext?.inputItems[0] as? NSExtensionItem {
-
+			
 			for ele in item.attachments!{
 				
 				let itemProvider = ele as! NSItemProvider
 				
 				if itemProvider.hasItemConformingToTypeIdentifier("public.png"){
 					print(itemProvider)
-//					itemProvider.loadItem(forTypeIdentifier: "public.jpeg", options: nil, completionHandler: { (item, error) in
-//						do{
-//							var imgData: Data!
-//							if let url = item as? URL{
-//								imgData = try Data(contentsOf: url)
-//							}
-//
-//							if let img = item as? UIImage{
-//								imgData = UIImagePNGRepresentation(img)
-//							}
-//							print("imgData =  \(imgData)")
-//						} catch let err{
-//							print(err)
-//						}
-//					})
+				
 					itemProvider.loadItem(forTypeIdentifier: "public.png", options: nil, completionHandler: {(item,error) in
-						do{
-							var imgData = UIImage()
-//							if let data = item {
-//								imgData = UIImage(data: NSData(contentsOfURL: data as! NSURL)!)
-//							}
-							if let url = item as? NSURL{
-								if let data = NSData(contentsOf: url as URL){
-									imgData = UIImage(data: data as Data)!
-								}
+						
+						var imgData = UIImage()
+						
+						if let url = item as? NSURL{
+							if let data = NSData(contentsOf: url as URL){
+								imgData = UIImage(data: data as Data)!
 							}
-
-							if let uploadImgData = UIImagePNGRepresentation(imgData) {
-								
-								self.uploadImage(data: uploadImgData)
-								let dict: [String : Any] = ["imgData" :  uploadImgData, "name" : self.contentText]
-								let userDefault = UserDefaults(suiteName: "group.DroneSimulator")
-//								userDefault.addSuite(named: "group.DroneSimulator")
-								userDefault?.set(dict, forKey: "imgData")
-								userDefault?.synchronize()
-								let dicData = userDefault?.value(forKey: "imgData") as? NSDictionary
-								
-								
-								self.testAppGroup(dic: dicData!)
-							}
-							
-//							print("imgData = \(imgData), item = \(item)")
-							
-						} catch let error {
-							print(error)
 						}
+						
+						if let uploadImgData = UIImagePNGRepresentation(imgData) {
+							
+							self.uploadImage(data: uploadImgData)
+							let dict: [String : Any] = ["imgData" :  uploadImgData, "name" : self.contentText]
+							let userDefault = UserDefaults(suiteName: "group.DroneSimulator")
+							//								userDefault.addSuite(named: "group.DroneSimulator")
+							userDefault?.set(dict, forKey: "imgData")
+							userDefault?.synchronize()
+							let dicData = userDefault?.value(forKey: "imgData") as? NSDictionary
+							print("userDefault saved")
+						}
+				
 					})
 				}
 				else	{
@@ -86,35 +63,47 @@ class ShareViewController: SLComposeServiceViewController {
 		self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
 	}
 	override func configurationItems() -> [Any]! {
-// To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+		// To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
 		return []
 	}
 	func uploadImage(data:Data){
 		var params: Parameters = [
 			"file" : data
 		]
-
+		
 		print("data = \(data)")
 		Alamofire.upload(
 			multipartFormData: { multipartFormData in
-				multipartFormData.append(data, withName:"file", fileName: "extension123.png", mimeType: "image/png")
+				multipartFormData.append(data, withName:"file", fileName: "extension1234.png", mimeType: "image/png")
 		},
 			to : "http://neinsys.io:5000/insertImagePost",
 			encodingCompletion: { encodingResult in
 				switch encodingResult {
 				case .success(let upload, _, _):
-					upload.responseString { response in
-						debugPrint(response.result)
-					}
-					print("success")
+					upload.responseJSON { response in
+						
+						debugPrint(response.result.value)
+//						if let json = response.result.value {
+//							if let objArr = json as? [Any] {
+//								print("objArr = \(objArr)")
+//								if let object = objArr[0] as? NSObject{
+//									print(object)
+//									if let dic = object as? [String:Any]{
+////										if let id = dic["_id"] as? String {
+////											print()
+////										}
+//										print("dic = \(dic["_id"])")
+//									}
+//								}
+//							}
+//						}
+//						print("Success!")
+					}					
+					
 				case .failure(let encodingError):
 					print("fail! \(encodingError)")
 				}
 		}
 		)
-	}
-	func testAppGroup(dic:NSDictionary){
-		
-		debugPrint("test App group : \(dic)")
 	}
 }
